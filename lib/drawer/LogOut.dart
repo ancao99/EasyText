7f35732a19ab81c52e50111d019faf42a11a568a
AnC <109709291+ancao99@users.noreply.github.com>
2023-07-17 11:22:06 -0400

    Update LogOut.dart

--- lib/drawer/LogOut.dart
+++ lib/drawer/LogOut.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class LogOut extends StatelessWidget {
   const LogOut({Key? key}) : super(key: key);
 
+  // Function to handle the log-out process
+  void _handleLogOut(BuildContext context) {
+    // TODO: Add your log-out logic here (e.g., clear authentication tokens, reset app state)
+    // For demonstration purposes, this example simply navigates back to the login page.
+    Navigator.pushNamedAndRemoveUntil(context, '/loginPage', (route) => false);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,51 +20,30 @@ class LogOut extends StatelessWidget {
         ),
         backgroundColor: Colors.blue[100],
       ),
-      body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/logout.jpg'),
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'You are logged out',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                    ),
-                    const Text(
-                      'Do you want to sign in? ',
-                      style: TextStyle(
-                        fontSize: 20.0,
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        print('Sign In');
-                        Navigator.pushNamed(context, '/registerPage');
-                      },
-                      child: const Text('Sign In'),
-                    ),
-                  ],
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Do you want to log out?',
+                style: TextStyle(
+                  fontSize: 20.0,
                 ),
               ),
-            ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  // Call the log-out function when the "Log Out" button is pressed.
+                  _handleLogOut(context);
+                },
+                child: const Text('Log Out'),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
-
 }
